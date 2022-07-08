7839b2009b626bd6065f6e12113cbead680fe854
inkoz <inkoz@DESKTOP-JS5JFS1>
2022-07-08 20:54:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork6
+{
+    internal class Program
+    {
+
+     
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите элементы: ");
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Кол-во элементов > 0: {count}");
+            Console.Read();
+        }
+    }
+}
